e1fac1d99a04801f38c92c1931b130e1fe80d445
田鹏程 <nulitin@126.com>
2026-08-17 10:11:51 +0800

    test

--- HelpForm.Designer.cs
+++ HelpForm.Designer.cs
@@ -45,7 +45,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(117, 81);
+            this.label2.Location = new System.Drawing.Point(115, 82);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(137, 12);
             this.label2.TabIndex = 1;
